0baf1a75dd03422a6f141d5b08de2762bc1c770e
Dr. Cat <47220664+catsmells@users.noreply.github.com>
2024-07-20 20:22:53 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='svor_analysis',
+    version='0.1.0',
+    description='A Python package for performing SVOR analysis!',
+    author='Dr. Cat',
+    author_email='j@drcat.fun',
+    packages=find_packages(),
+    install_requires=[],
+)
